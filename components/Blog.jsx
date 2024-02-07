3f6e8aba1e56348e73e92d7bdc2fc7b55e1b564f
bilal <suzzanebs123@gmail.com>
2024-02-08 04:24:48 +0530

    final test for productin

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -7,43 +7,42 @@ import img3 from "../src/assets/images/blog3.jpg";
 import img4 from "../src/assets/images/blog4.jpg";
 import img5 from "../src/assets/images/blog5.jpg";
 import img6 from "../src/assets/images/blog6.jpg";
- 
 
 const Blogs = () => {
-    return (
-      <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-24">
-        <div className=" flex flex-col items-center lg:flex-row justify-between">
-          <div>
-            <h1 className=" text-4xl font-semibold text-center lg:text-start">
-              Latest Post
-            </h1>
-            <p className=" mt-2 text-center lg:text-start">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
-              quidem.
-            </p>
-          </div>
-          <div className=" mt-4 lg:mt-0">
-            <Button title="Our Articles" />
-          </div>
+  return (
+    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-24">
+      <div className=" flex flex-col items-center lg:flex-row justify-between">
+        <div>
+          <h1 className=" text-4xl font-semibold text-center lg:text-start">
+            Latest Post
+          </h1>
+          <p className=" mt-2 text-center lg:text-start">
+            Age-related wear and tear, leading to degeneration of the cervical
+            spine, including the discs and joints
+          </p>
         </div>
-        <div className=" my-8">
-          <div className=" flex flex-wrap justify-center gap-7">
-            <BlogCard img={img1} headlines="Unraveling the Mysteries of Sleep" />
-            <BlogCard img={img2} headlines="The Heart-Healthy Diet" />
-            <BlogCard
-              img={img3}
-              headlines="Understanding Pediatric Vaccinations"
-            />
-            <BlogCard img={img4} headlines="Navigating Mental Health" />
-            <BlogCard img={img5} headlines="The Importance of Regular Exercise" />
-            <BlogCard img={img6} headlines="Skin Health 101" />
-          </div>
+        <div className=" mt-4 lg:mt-0">
+          <Button title="Our Articles" />
         </div>
       </div>
-    );
-  };
-  
-  export default Blogs;
+      <div className=" my-8">
+        <div className=" flex flex-wrap justify-center gap-7">
+          <BlogCard img={img1} headlines="Unraveling the Mysteries of Sleep" />
+          <BlogCard img={img2} headlines="The Heart-Healthy Diet" />
+          <BlogCard
+            img={img3}
+            headlines="Understanding Pediatric Vaccinations"
+          />
+          <BlogCard img={img4} headlines="Navigating Mental Health" />
+          <BlogCard img={img5} headlines="The Importance of Regular Exercise" />
+          <BlogCard img={img6} headlines="Skin Health 101" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
 
 // Unraveling the Mysteries of Sleep
 
